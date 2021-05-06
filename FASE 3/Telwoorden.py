1d1438e26fff8ff45d4a938b868ba697ad5b78b8
isismelissen <83287039+isismelissen@users.noreply.github.com>
2021-05-06 13:46:28 +0200

    Add files via upload

--- FASE 3/Telwoorden.py
+++ FASE 3/Telwoorden.py
@@ -1,21 +1,27 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu May  6 00:47:02 2021
+Created on Thu May  6 13:45:05 2021
 
-@author: 20202139
+@author: 20202876
 """
 
-from collections import Counter
 
-def Telwoorden(bestandsnaam):
-    infile = open(bestandsnaam)
-    inlines = infile.read().splitlines()
-    infile.close()
+def TelWoorden(filename):
     
-    for line in range(len(inlines)):
-        print(inlines[line].replace("\\", " "))
-   
-    print(Counter(inlines))
+    inFile = open(filename)
+    raw_data = inFile.readlines()
+    inFile.close()
 
-Telwoorden('GenDescription2.txt')
+    data = [[line.strip()] for line in raw_data[1::3]]
+    
+    desc_freq = {}
+    
+    for description in data:
+        if description not in desc_freq:
+            desc_freq[description] = 1
+        else:
+            desc_freq[description] += 1
             
+    desc_freq = {keys: values for keys, values in sorted(desc_freq.items(), key=lambda item: item[1])}
+    
+    return desc_freq
