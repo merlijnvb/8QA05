c2f9463fe0ee53d69cc1bf591bfdbd2e4908e76c
FemkeSchapendonk <83287349+FemkeSchapendonk@users.noreply.github.com>
2021-05-11 11:14:12 +0200

    Add files via upload

--- FASE 3/Interpertatie TelWoorden.py
+++ FASE 3/Interpertatie TelWoorden.py
@@ -50,5 +50,51 @@ def TelWoorden(bestandsnaam):
     print(len(dic.keys()))
 
 TelWoorden('GenDescription2.txt')
+
+
+def descriptionCloneIDs(bestand1, bestand2):
     
+    #open gen_description en voorbeeld_cluster_result bestanden
+    inFile = open(bestand1)
+    raw_data = inFile.readlines()
+    inFile.close
     
+    voorbeeld_cluster_result = open(bestand2)
+    raw_cluster_data = voorbeeld_cluster_result.readlines()
+    voorbeeld_cluster_result.close()
+    
+    #maak lijst met de beschrijving en overeenkomende clone IDs
+    description = [line.strip()for line in raw_data [1::3]]
+    cloneIDs = [line.strip() for line in raw_data [0::3]]
+   
+    #maak lege dictionairy genaamd desciptionCloneIDs
+    descriptionCloneIDs = {}
+    
+    #alle cloneIDs overeenkomend met die beschrijving 
+    #all cloneIDs corresponding to that description
+    for item in range(len(description)):
+        if description[item] not in descriptionCloneIDs:
+            descriptionCloneIDs[description[item]] = [cloneIDs[item]]
+        else:
+            descriptionCloneIDs[description[item]].append[cloneIDs[item]]
+   
+    #maak lege dictionairy genaamd cluster
+    clusterFreq = {}
+    
+    cluster_data = [items for sublist in [line.strip().split for line in raw_cluster_data] for item in sublist]
+    
+    cloneIDValues = list(descriptionCloneIDs.values())
+    cloneIDKeys = list(descriptionCloneIDs.keys())
+    
+    for description in range(len(cloneIDsValues[description])):
+        if cloneIDValues[description][cloneID] in cluster_data:
+            cluster_index = cluster_data.index(cloneIDValues[description][cloneID]) + 1
+            cluster_value = int(cluster_data[cluster_index])
+            clusterFreq[cloneIDKeys[description]][cluster_value] += 1
+            
+        else: 
+            pass
+    print(clusterFreq)
+    return clusterFreq
+
+descriptionCloneIDs(bestand1='GenDescription2.txt',bestand2='Voorbeeld_clusterresult.txt')
