73fd07360fe9517fc9b5fa3e8554cefac21eda97
elizabethninh <83179466+elizabethninh@users.noreply.github.com>
2021-05-17 17:26:25 +0200

    Add files via upload

--- FASE 3/Fase 3.py
+++ FASE 3/Fase 3.py
@@ -8,7 +8,7 @@ Created on Mon May 17 13:06:34 2021
 # BESTANDEN INLEZEN 
 # GenDescription2.txt inlezen
 infile_GD = open('GenDescription2.txt')
-inlines_GD = infile_GD.readlines()
+inlines_GD = infile_GD.read().replace("\\", "").splitlines()
 infile_GD.close()
     
 # Voorbeeld_clusterresult.txt inlezen
@@ -16,8 +16,7 @@ infile_VC = open('Voorbeeld_clusterresult.txt')
 inlines_VC = infile_VC.readlines()
 infile_VC.close()
 
-
-
+    
 # TWEE DICTIONARIES MAKEN: CLONEID-BESCHRIJVING & CLONEID-CLUSTER
 # dictionary CloneID - Beschrijving
 i = 1
@@ -25,9 +24,8 @@ beschrijvingdict = {}
 
 for i in range(len(inlines_GD)):                # i moet een regel zijn in de regels van het bestand
     beschrijving = inlines_GD[i]                # beschrijving is regel 2 van het bestand
-    beschrijving = beschrijving.lower()         # alle woorden lowercase
     beschrijving = beschrijving.split()         # alle woorden in de beschrijving los
-    cloneID = inlines_GD[i-1]                   # van een integer terug naar een string
+    cloneID = str(inlines_GD[i-1])              # regel 0
     beschrijvingdict[cloneID] = beschrijving    # beschrijving wordt value van key = cloneID
     i=i+3                                       # 'verpakt' per 3 regels
     
@@ -38,7 +36,6 @@ for line in inlines_VC:                         # voor iedere regel in regels va
     regel = line.split()                        # alle ID's en clusternummer los van elkaar
     cloneID.append(regel[0])                    # voeg het ID van VC (positie 0) samen met het ID van GD
     cloneIDdict[regel[0]]= int(regel[1])        # clusternummer als integer wordt value van de key cloneID
-
 
 
 print(beschrijvingdict)
