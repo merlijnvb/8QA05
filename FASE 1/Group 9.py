1aaedbcbfa519d78d21b6c3ff4002b4aaaae2760
Niels van Noort <83238451+niels522@users.noreply.github.com>
2021-05-27 11:19:39 +0200

    Add files via upload

---
+++ FASE 1/Group 9.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 27 11:03:01 2021
+
+@author: 20202309
+"""
+
+
+import Fase1 as F1
+
+outf_name = F1.main()
+
+print("The data has been written to",outf_name)
