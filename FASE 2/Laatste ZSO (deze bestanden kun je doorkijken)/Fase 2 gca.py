736d952f1c954946eb58a9380994cb7d920da9f6
Merlijn van Benthem <74921668+merlijnvb@users.noreply.github.com>
2021-05-28 16:22:29 +0200

    Add files via upload

--- FASE 2/Laatste ZSO (deze bestanden kun je doorkijken)/Fase 2 gca.py
+++ FASE 2/Laatste ZSO (deze bestanden kun je doorkijken)/Fase 2 gca.py
@@ -26,15 +26,17 @@ def file_to_lib(FileName):
     return lib
 
 class GCA:
-    def __init__(self, scope=1, subspaces=9, tresh=3, data={}):
+    def __init__(self, scope=1, subspaces=9, thres=3, data={}):
         self.scope = scope
         self.subspaces = subspaces 
-        self.tresh = tresh 
+        self.thres = thres 
         self.lib_data = data
         self.lib_axis = {}
         self.lib_interval = {}
         self.lib_cells = {}
         self.lib_grid = {}
+        self.lib_clustered = {}
+        self.indexes_not_clusterd = np.array(list(self.lib_data.keys()))
         
     def interval_data(self, data={}):
         if self.lib_data == {}:
@@ -72,9 +74,121 @@ class GCA:
             self.lib_grid[index] = np.zeros((len(self.lib_interval),),dtype=int)
             
             for axis in grid:
-                self.lib_grid[index][axis] = grid[axis][i]            
+                self.lib_grid[index][axis] = grid[axis][i]        
         
+    
+    
+    def assign_neigbours(self, index0, cluster):
+        neighbours = []
+    
+        for index in self.indexes_not_clusterd:
+            (np.subtract(self.lib_grid[index], self.lib_grid[index0]))
+            if all([-self.scope <= axis_distance <= self.scope for axis_distance in np.subtract(self.lib_grid[index], self.lib_grid[index0])]):
+                neighbours.append(index)
+                cluster.append(index)
+                self.indexes_not_clusterd = np.delete(self.indexes_not_clusterd, np.where(self.indexes_not_clusterd == index))          
+        
+        if len(neighbours) > 1:
+             for neighbour in neighbours:
+                 self.assign_neigbours(neighbour,cluster)
+        
+        
+    
+    def clustering(self, data={}):
+        if self.lib_data == {}:
+            if data == {}:
+                raise ImportError("No input given. --> clustering(data) or __init__(data) or interval_data(data)")
+            else:
+                self.lib_data = data
+                self.indexes_not_clusterd = np.array(list(self.lib_data.keys()))
+        if (self.indexes_not_clusterd.size <= 1) & (self.lib_data != {}):
+            self.indexes_not_clusterd = np.array(list(self.lib_data.keys()))
+            
+        cluster_nr = 0
+        anticluster_nr = 0
+        lib_anticlustered = {}
+        
+        for index in self.indexes_not_clusterd:
+            if index in self.indexes_not_clusterd:
+                cluster = []
+                self.assign_neigbours(index,cluster)
+                
+                if len(cluster) > self.thres:                 
+                    self.lib_clustered[cluster_nr] = cluster
+                    cluster_nr += 1
+                else:
+                    lib_anticlustered[anticluster_nr] = cluster
+                    anticluster_nr += 1
+                    
+        self.lib_clustered[-1] = lib_anticlustered     
+        
+        return self.lib_clustered
+    
+    def silhouette_score(self):
+        '''
+        Postconditions: silhouette_score => score how good the k-means clustering fit is
+        Task of function: calculating a score using a better scoring system with more indicative score values:
+            --> A small internal dissimilarity value means it is well matched. Furthermore, a large external dissimilarity value means it is badly matched to its neighbouring cluster.
+            
+            --> Therefor if silhouette_score is closer to -1     ==>     datapoint would be better assigned to another cluster
+            --> Therefor if silhouette_score is closer to  1     ==>     datapoint is appropriatly assigned to cluster
+            --> If silhouette_score is close to 0     ==>     datapoint is on the border of two clusters.
+        '''
+        list_scores = []
+        
+        for index in self.lib_data:
+            cluster_nr = [cluster for cluster, indexes in self.lib_clustered.items() if (index in indexes) and (cluster != -1)] # GET CLUSTER #NR THE PROTEIN IS ASSIGNED TO
+            
+            if cluster_nr != []:
+                cluster_nr = cluster_nr[0]
+            
+                if len(self.lib_clustered[cluster_nr]) > 1:
+                    # CALCULATE THE MEAN DISTANCE OF THE PROTEIN TO ALL THE OTHER PROTEINS IN ITS CLUSTER (INTERNAL):
+                    dissimilarity_internal = np.sum([np.linalg.norm(self.lib_data[index] - self.lib_data[index_in]) for index_in in self.lib_data]) / (len(self.lib_data)-1)
+                    
+                    # CALCULATE THE MINIMAL MEAN DISTANCE OF THE PROTEIN TO ALL THE OTHER PROTEINS FROM THE OTHER CLUSTERS (EXTERNAL):
+                    # --> MEAN DISTANCE IS GROUPED PER CLUSTER. FROM THIS LIST THE MINIMAL DISTANCE IS CALCULATED
+                    dissimilarity_external = np.min([np.mean([np.linalg.norm(self.lib_data[index] - self.lib_data[index_ex]) for index_ex in self.lib_data if index_ex in self.lib_clustered[cluster]]) for cluster in self.lib_clustered if (cluster != cluster_nr) & (cluster != -1)])
+                    
+                    # CALCULATING THE SILHOUETTE SCORE FOR PROTEIN BY APPLYING THE FORMULA:
+                    sil_score_i = (dissimilarity_external - dissimilarity_internal) / max(dissimilarity_external,dissimilarity_internal)
+                    
+                if len(self.lib_clustered[cluster_nr]) == 1:
+                    sil_score_i = 0
+                
+                list_scores.append(sil_score_i)
+            
+        self.Sil_score = np.mean(list_scores)
+        
+        if np.isnan(self.Sil_score):
+            self.Sil_score = float('-inf')
+                
+        return self.Sil_score
+    
+    def Escore(self):
+        '''
+        Postconditions: E_score => score how good the k-means clustering fit is
+        Task of function: calculation the goodness of the k-means clustering application of certain centroids
+        '''
+        
+        summation = 0
+        lib_centroids = {}
+        
+        ## HIER VERDER WERKEN
+        for k in self.lib_clustered:
+            if k >= 0:
+                for axis in self.lib_axis:
+                    print([index for index in self.lib_axis[axis] if index in self.lib_clustered[k]])
 
+        # for k in range(self.k): # CALCULATE FOR EVERY CLUSTER THE E_SCORE
+        #     summation += np.sum([np.square(abs(np.subtract(self.lib_centroid[k], self.lib_data[dis]))) for dis in self.lib_clustered[k]])
+            
+        # # CALCULATE THE MEAN E_SCORE FOR THE ENTIRE K-MEANS FIT
+        # E_score = summation / self.k
+        
+        # return E_score
+        
+    
 
 lib_data = file_to_lib('Data\Voorbeeld_clusterdata.txt')
 lib_results = file_to_lib('Data\Voorbeeld_clusterresult.txt')
@@ -82,3 +196,7 @@ lib_results = file_to_lib('Data\Voorbeeld_clusterresult.txt')
 gca = GCA()
 gca.interval_data(lib_data)
 gca.grid_data()
+results = gca.clustering()
+for i in results:
+    print(len(results[i]))
+print(gca.Escore())
